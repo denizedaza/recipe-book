6645d5aa73d747e0382fc3dab5f9fb1d9dc146e2
denizedaza <dazadenize@gmail.com>
2022-01-06 19:48:25 -0500

    add event emitter and new sample data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeClicked = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('sample recipe', 'a test recipe descriptor (Japanese Cheesecake)', 'https://www.justonecookbook.com/wp-content/uploads/2019/11/Japanese-Cheesecake-4631-I-500x500.jpg')
+    new Recipe('sample recipe', 'a test recipe descriptor (Japanese Cheesecake)', 'https://www.justonecookbook.com/wp-content/uploads/2019/11/Japanese-Cheesecake-4631-I-500x500.jpg'),
+    new Recipe('a second recipe', 'the second recipe descriptor (Squash Soup)', 'https://www.jessicagavin.com/wp-content/uploads/2020/11/acorn-squash-soup-14-1200.jpg')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeClicked.emit(recipe);
   }
 
 }

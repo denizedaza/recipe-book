4fc9295c4c2284d46b086175b67606cf900fa5d1
denizedaza <dazadenize@gmail.com>
2022-01-10 15:41:47 -0500

    create and implement shopping list service

--- src/app/shopping-list/shopping-list-service.ts
+++ src/app/shopping-list/shopping-list-service.ts
@@ -1,3 +1,16 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class ShoppingListService {
-  
+  private ingredients: Ingredient[] = [
+    new Ingredient('apples', 5),
+    new Ingredient('cinnamon', 5),
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }

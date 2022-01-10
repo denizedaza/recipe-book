91797e1042bb2d84c53dcfc3ed3d8194df847835
denizedaza <dazadenize@gmail.com>
2022-01-10 16:15:20 -0500

    update recipe model

--- src/app/recipes/recipes.services.ts
+++ src/app/recipes/recipes.services.ts
@@ -1,12 +1,40 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipesService{
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('sample recipe', 'a test recipe descriptor (Japanese Cheesecake)', 'https://www.justonecookbook.com/wp-content/uploads/2019/11/Japanese-Cheesecake-4631-I-500x500.jpg'),
-    new Recipe('a second recipe', 'the second recipe descriptor (Squash Soup)', 'https://www.jessicagavin.com/wp-content/uploads/2020/11/acorn-squash-soup-14-1200.jpg')
+    new Recipe(
+      'Japanese Cheesecake',
+      'a lovely fluffy cheesecake - Japanese style!',
+      'https://www.justonecookbook.com/wp-content/uploads/2019/11/Japanese-Cheesecake-4631-I-500x500.jpg',
+      [
+        new Ingredient('unsalted butter', 1),
+        new Ingredient('eggs', 1),
+        new Ingredient('cream cheese', 2),
+        new Ingredient('heavy whipping cream', 1),
+        new Ingredient('granulated sugar', 1),
+        new Ingredient('cake clour', 1),
+        new Ingredient('lemon', 1),
+        new Ingredient('apricot jam', 1),
+      ]),
+    new Recipe(
+      'Squash Soup',
+      'warm and cool and cozy squash soup',
+      'https://www.jessicagavin.com/wp-content/uploads/2020/11/acorn-squash-soup-14-1200.jpg',
+      [
+        new Ingredient('butternut squash', 1),
+        new Ingredient('olive oil - tablespoon', 1),
+        new Ingredient('chopped shallot - 1/2 cup', 1),
+        new Ingredient('garlic cloves', 4),
+        new Ingredient('maple syrup', 1),
+        new Ingredient('ground nutmeg', 1),
+        new Ingredient('ground black pepper', 1),
+        new Ingredient('vegetable broth', 3),
+        new Ingredient('butter', 1),
+      ])
   ];
 
   getRecipes() {

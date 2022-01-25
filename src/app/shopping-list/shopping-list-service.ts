485d581297145c261590f59c2bd6a5749eb67b9a
denizedaza <dazadenize@gmail.com>
2022-01-25 14:08:20 -0500

    fix update ingredient item

--- src/app/shopping-list/shopping-list-service.ts
+++ src/app/shopping-list/shopping-list-service.ts
@@ -27,4 +27,9 @@ export class ShoppingListService {
     this.ingredients.push(...ingredientsToAdd);
     this.ingredientsChanged.next(this.ingredients.slice());
   }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice())
+  }
 }

a4b8aa698318778b202ef54de6ca4699bb9724e9
denizedaza <dazadenize@gmail.com>
2022-10-27 16:44:26 -0400

    create data resolver

---
+++ src/app/recipes/recipes-resolver.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { DataStorageService } from '../shared/data-storage.service';
+import { Recipe } from './recipe.model';
+import { RecipesService } from './recipes.services';
+
+@Injectable({ providedIn: 'root' })
+export class RecipeResolverService implements Resolve<Recipe[]> {
+  constructor(
+    private dataStorageService: DataStorageService,
+    private recipesService: RecipesService
+  ) {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const recipes = this.recipesService.getRecipes();
+    if (recipes.length === 0) {
+      return this.dataStorageService.fetchRecipes();
+    } else {
+      return recipes;
+    }
+  }
+}

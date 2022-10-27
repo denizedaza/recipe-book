e1ac57bc705c0b25c6caadae8984f4529b830fde
denizedaza <dazadenize@gmail.com>
2022-10-27 16:47:21 -0400

    fix fetch subscription for data storage

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipesService } from '../recipes/recipes.services';
 import { Recipe } from '../recipes/recipe.model';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -23,12 +24,22 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
+    return this.http
       .get<Recipe[]>(
         'https://recipe-book-d5f15-default-rtdb.firebaseio.com/recipes.json'
       )
-      .subscribe((recipes) => {
-        this.recipeService.setRecipes(recipes);
-      });
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        }),
+        tap((recipe) => {
+          this.recipeService.setRecipes(recipe);
+        })
+      );
   }
 }
